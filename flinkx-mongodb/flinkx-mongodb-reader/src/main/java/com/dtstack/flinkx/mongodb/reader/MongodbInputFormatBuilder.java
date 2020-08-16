0ebb044831af80ce52e241a4e0cf6b33ebd8ebcf
jack <13126506250@163.com>
2020-08-16 19:07:58 +0800

    update mongodb reader

--- flinkx-mongodb/flinkx-mongodb-reader/src/main/java/com/dtstack/flinkx/mongodb/reader/MongodbInputFormatBuilder.java
+++ flinkx-mongodb/flinkx-mongodb-reader/src/main/java/com/dtstack/flinkx/mongodb/reader/MongodbInputFormatBuilder.java
@@ -22,6 +22,7 @@ import com.dtstack.flinkx.constants.ConstantValue;
 import com.dtstack.flinkx.inputformat.BaseRichInputFormatBuilder;
 import com.dtstack.flinkx.mongodb.MongodbConfig;
 import com.dtstack.flinkx.reader.MetaColumn;
+import org.bson.json.JsonWriterSettings;
 
 import java.util.List;
 
@@ -61,4 +62,19 @@ public class MongodbInputFormatBuilder extends BaseRichInputFormatBuilder {
             throw new IllegalArgumentException("批量读取条数必须小于[200000]条");
         }
     }
+
+    // bson转json格式设置
+    public static JsonWriterSettings getJsonWriterSettings() {
+        JsonWriterSettings jsonWriterSettings = JsonWriterSettings.builder()
+                .dateTimeConverter((value, writer) -> writer.writeString(Long.toString(value)))
+                .decimal128Converter((value, writer) -> writer.writeNumber(value.toString()))
+                .objectIdConverter((value, writer) -> writer.writeString(value.toString()))
+                .int32Converter((value, writer) -> writer.writeNumber(value.toString()))
+                .int64Converter((value, writer) -> writer.writeString(Long.toString(value)))
+                .build();
+
+        return jsonWriterSettings;
+    }
+
+
 }
